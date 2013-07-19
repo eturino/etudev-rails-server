aa1eb823a196739339a8f5cd975a4e158a1d538f
Eduardo Turiño <eturino@gmail.com>
2013-07-19 08:19:12 +0200

    default db_root_password: - nil for default (need to be in the ROLE or NODE files) - root for vagrant

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
+# DATABASES
+default['databox']['db_root_password'] = nil
+
 # RUBY
 default['etudev-rails-server']['ruby']['versions'] = %w(2.0.0-p247 1.9.3-p392)
 default['etudev-rails-server']['ruby']['global_version'] = '1.9.3-p392'
--- recipes/vagrant.rb
+++ recipes/vagrant.rb
@@ -1,5 +1,7 @@
 #modifications for Vagrant server
 
+node.default["databox"]["db_root_password"] = "root"
+    
 node.default["authorization"]["sudo"]["users"] = ["vagrant"]
 
 node.default["etudev-rails-server"]["bashit_users"] = ["vagrant"]
