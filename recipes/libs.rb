29599794100f98e1230e02522cb6d07ff08e91e3
Eduardo Turiño <eturino@gmail.com>
2013-07-20 21:49:24 +0200

    lib for charcoal

--- recipes/libs.rb
+++ recipes/libs.rb
@@ -1,7 +1,7 @@
 packages = value_for_platform( ["redhat", "centos", "scientific", "amazon", "oracle"] => {
-    "default" => %w(ruby-devel curl-devel sqlite-devel) },
+    "default" => %w(ruby-devel curl-devel sqlite-devel libicu-devel) },
                                ["ubuntu", "debian"] => {
-                                   "default" => %w(ruby-dev libcurl4-gnutls-dev libsqlite3-dev) } )
+                                   "default" => %w(ruby-dev libcurl4-gnutls-dev libsqlite3-dev libicu-dev) } )
 
 packages.each do |devpkg|
   package devpkg
